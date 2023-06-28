2f7d0a67ef57fcdc1f73051ecb7c45cfce703a48
Anders Wenhaug <awenhaug@gmail.com>
2023-06-28 15:56:06 +0200

    Fix not being able to convert char* to string

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
+#include <string>
 #include "Computer.h"
 
 int main(int argc, char **argv) {
     Computer computer;
 
     if (argc > 1) {
-        computer.load(argv[1]);
+        computer.load(std::string(argv[1]));
     }
 
     computer.run();
